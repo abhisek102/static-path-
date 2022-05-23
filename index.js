30d647c1536f65d23a659920cabd7107717b0d4d
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-23 14:20:59 +0530

    2 files modified, 3 added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8000;
+const staticpath = path.join(__dirname, '/project');
+app.use(express.static(staticpath));
+app.listen(port);
